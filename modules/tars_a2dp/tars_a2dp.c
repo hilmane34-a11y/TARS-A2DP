3c3435fe742e56da7f05ec46f25713ee518ba307
hilmane34-a11y <hilmane34@gmail.com>
2026-08-29 21:43:33 +0700

    Implement TARS A2DP module with test function

---
+++ modules/tars_a2dp/tars_a2dp.c
@@ -0,0 +1,29 @@
+#include "py/runtime.h"
+
+STATIC mp_obj_t tars_a2dp_test(void) {
+    return mp_obj_new_str("TARS A2DP MODULE READY", 22);
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(
+    tars_a2dp_test_obj,
+    tars_a2dp_test
+);
+
+STATIC const mp_rom_map_elem_t tars_a2dp_globals_table[] = {
+    { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_tars_a2dp) },
+    { MP_ROM_QSTR(MP_QSTR_test), MP_ROM_PTR(&tars_a2dp_test_obj) },
+};
+
+STATIC MP_DEFINE_CONST_DICT(
+    tars_a2dp_globals,
+    tars_a2dp_globals_table
+);
+
+const mp_obj_module_t tars_a2dp_user_cmodule = {
+    .base = { &mp_type_module },
+    .globals = (mp_obj_dict_t *)&tars_a2dp_globals,
+};
+
+MP_REGISTER_MODULE(
+    MP_QSTR_tars_a2dp,
+    tars_a2dp_user_cmodule
+);
