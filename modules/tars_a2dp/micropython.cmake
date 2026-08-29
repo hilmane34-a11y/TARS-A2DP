542c43264e16ed4673c794f2ccd6d2d0fe4bbfd4
hilmane34-a11y <hilmane34@gmail.com>
2026-08-30 02:53:43 +0700

    Add CMake configuration for usermod_tars_a2dp
    Define usermod_tars_a2dp library and link with ESP-IDF Bluetooth.

---
+++ modules/tars_a2dp/micropython.cmake
@@ -0,0 +1,19 @@
+add_library(usermod_tars_a2dp INTERFACE)
+
+target_sources(usermod_tars_a2dp INTERFACE
+    ${CMAKE_CURRENT_LIST_DIR}/tars_a2dp.c
+)
+
+target_include_directories(usermod_tars_a2dp INTERFACE
+    ${CMAKE_CURRENT_LIST_DIR}
+)
+
+# Hubungkan modul dengan komponen Bluetooth ESP-IDF
+target_link_libraries(usermod_tars_a2dp INTERFACE
+    __idf_bt
+)
+
+# Daftarkan modul ke MicroPython
+target_link_libraries(usermod INTERFACE
+    usermod_tars_a2dp
+)
