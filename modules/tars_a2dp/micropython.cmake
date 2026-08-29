4db6e08b056e8d249ccad3df27b4fd728feac3c4
hilmane34-a11y <hilmane34@gmail.com>
2026-08-30 00:30:32 +0700

    Add CMake configuration for usermod_tars_a2dp

---
+++ modules/tars_a2dp/micropython.cmake
@@ -0,0 +1,11 @@
+add_library(usermod_tars_a2dp INTERFACE)
+
+target_sources(usermod_tars_a2dp INTERFACE
+    ${CMAKE_CURRENT_LIST_DIR}/tars_a2dp.c
+)
+
+target_include_directories(usermod_tars_a2dp INTERFACE
+    ${CMAKE_CURRENT_LIST_DIR}
+)
+
+target_link_libraries(usermod INTERFACE usermod_tars_a2dp)
