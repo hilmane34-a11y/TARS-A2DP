baaf1eb57ac8095947a9746c831dc9a4da5f1856
hilmane34-a11y <hilmane34@gmail.com>
2026-08-30 12:24:47 +0700

    Implement test function for TARS Bluetooth module
    Added a test function to the TARS Bluetooth module that returns a readiness message.

---
+++ modules/tars_bluetooth/tars_bluetooth.c
@@ -0,0 +1,35 @@
+#include "py/runtime.h"
+
+static mp_obj_t tars_bluetooth_test(void) {
+    return mp_obj_new_str("TARS BLUETOOTH READY", 20);
+}
+static MP_DEFINE_CONST_FUN_OBJ_0(
+    tars_bluetooth_test_obj,
+    tars_bluetooth_test
+);
+
+static const mp_rom_map_elem_t tars_bluetooth_globals_table[] = {
+    {
+        MP_ROM_QSTR(MP_QSTR___name__),
+        MP_ROM_QSTR(MP_QSTR_tars_bluetooth)
+    },
+    {
+        MP_ROM_QSTR(MP_QSTR_test),
+        MP_ROM_PTR(&tars_bluetooth_test_obj)
+    },
+};
+
+static MP_DEFINE_CONST_DICT(
+    tars_bluetooth_globals,
+    tars_bluetooth_globals_table
+);
+
+const mp_obj_module_t tars_bluetooth_user_cmodule = {
+    .base = { &mp_type_module },
+    .globals = (mp_obj_dict_t *)&tars_bluetooth_globals,
+};
+
+MP_REGISTER_MODULE(
+    MP_QSTR_tars_bluetooth,
+    tars_bluetooth_user_cmodule
+);
