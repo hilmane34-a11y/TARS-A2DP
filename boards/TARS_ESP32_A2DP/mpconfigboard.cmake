632d8b2e2ca3f6a4baa3adae7ff87f7bd1315a71
hilmane34-a11y <hilmane34@gmail.com>
2026-08-30 20:42:08 +0700

    Add custom partition table filename to config

--- boards/TARS_ESP32_A2DP/mpconfigboard.cmake
+++ boards/TARS_ESP32_A2DP/mpconfigboard.cmake
@@ -2,3 +2,7 @@ set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.base
     ${MICROPY_BOARD_DIR}/sdkconfig.bluetooth
 )
+
+set(PARTITION_TABLE_CUSTOM_FILENAME
+    "${MICROPY_BOARD_DIR}/partitions.csv"
+)
