c12741dca3fecd791271a93713183f4dce4dec8f
hilmane34-a11y <hilmane34@gmail.com>
2026-08-30 16:32:06 +0700

    Refactor to include micropython.cmake for modules

--- modules/micropython.cmake
+++ modules/micropython.cmake
@@ -1,9 +1,2 @@
-target_sources(usermod INTERFACE
-    ${CMAKE_CURRENT_LIST_DIR}/tars_a2dp/tars_a2dp.c
-    ${CMAKE_CURRENT_LIST_DIR}/tars_bluetooth/tars_bluetooth.c
-)
-
-target_include_directories(usermod INTERFACE
-    ${CMAKE_CURRENT_LIST_DIR}/tars_a2dp
-    ${CMAKE_CURRENT_LIST_DIR}/tars_bluetooth
-)
+include(${CMAKE_CURRENT_LIST_DIR}/tars_a2dp/micropython.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/tars_bluetooth/micropython.cmake)
