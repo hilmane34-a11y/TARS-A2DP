a839621efbd30f6057b642d0a2956f4aa0f7c396
hilmane34-a11y <hilmane34@gmail.com>
2026-08-30 13:30:14 +0700

    Define MODULE_TARS_A2DP_ENABLED in CMake configuration

--- modules/tars_a2dp/micropython.cmake
+++ modules/tars_a2dp/micropython.cmake
@@ -8,6 +8,10 @@ target_include_directories(usermod_tars_a2dp INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}
 )
 
+target_compile_definitions(usermod_tars_a2dp INTERFACE
+    MODULE_TARS_A2DP_ENABLED=1
+)
+
 target_link_libraries(usermod INTERFACE
     usermod_tars_a2dp
 )
