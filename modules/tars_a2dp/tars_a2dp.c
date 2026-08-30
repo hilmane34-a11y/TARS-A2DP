bc701e9de7c6031de180560688df607da39c920c
hilmane34-a11y <hilmane34@gmail.com>
2026-08-30 23:17:37 +0700

    Refactor A2DP event handling and improve readability

--- modules/tars_a2dp/tars_a2dp.c
+++ modules/tars_a2dp/tars_a2dp.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 #include "esp_err.h"
 #include "esp_bt.h"
@@ -35,9 +36,8 @@
    Little Endian
    Stereo
    44100 Hz
-
-   65536 bytes is about 0.37 seconds of stereo 44.1kHz audio.
 */
+
 #define PCM_BUFFER_SIZE        65536
 
 
@@ -136,14 +136,14 @@ static size_t tars_pcm_read(
     size_t len
 ) {
 
-    size_t read = 0;
+    size_t read_count = 0;
 
     while (
-        read < len &&
+        read_count < len &&
         pcm_used > 0
     ) {
 
-        data[read] =
+        data[read_count] =
             pcm_buffer[pcm_read_pos];
 
         pcm_read_pos++;
@@ -156,11 +156,11 @@ static size_t tars_pcm_read(
 
         pcm_used--;
 
-        read++;
+        read_count++;
 
     }
 
-    return read;
+    return read_count;
 
 }
 
@@ -182,19 +182,25 @@ static int32_t tars_a2dp_data_callback(
 ) {
 
     if (
-        !tars_a2dp_connected ||
+        data == NULL ||
         len <= 0
     ) {
+        return 0;
+    }
 
-        if (len > 0) {
-            memset(
-                data,
-                0,
-                len
-            );
-        }
+
+    if (
+        !tars_a2dp_connected
+    ) {
+
+        memset(
+            data,
+            0,
+            (size_t)len
+        );
 
         return len;
+
     }
 
 
@@ -233,6 +239,13 @@ static int32_t tars_a2dp_data_callback(
 
 /* =========================================================
    A2DP EVENT CALLBACK
+
+   IMPORTANT:
+   We intentionally use IF / ELSE instead of nested SWITCH
+   for the state values.
+
+   This avoids duplicate case value problems between
+   ESP-IDF versions/configurations.
    ========================================================= */
 
 static void tars_a2dp_event_callback(
@@ -240,136 +253,138 @@ static void tars_a2dp_event_callback(
     esp_a2d_cb_param_t *param
 ) {
 
-    switch (event) {
+    if (
+        param == NULL
+    ) {
+        return;
+    }
 
 
-        /* ---------------------------------------------
-           CONNECTION STATE
-           --------------------------------------------- */
+    /* ---------------------------------------------
+       CONNECTION STATE
+       --------------------------------------------- */
 
-        case ESP_A2D_CONNECTION_STATE_EVT:
+    if (
+        event == ESP_A2D_CONNECTION_STATE_EVT
+    ) {
 
-            switch (
-                param->conn_stat.state
+        esp_a2d_connection_state_t state =
+            param->conn_stat.state;
+
+
+        if (
+            state ==
+            ESP_A2D_CONNECTION_STATE_DISCONNECTED
+        ) {
+
+            tars_a2dp_connected =
+                false;
+
+            tars_a2dp_connecting =
+                false;
+
+            tars_status_text =
+                "A2DP DISCONNECTED";
+
+        }
+        else if (
+            state ==
+            ESP_A2D_CONNECTION_STATE_CONNECTING
+        ) {
+
+            tars_a2dp_connecting =
+                true;
+
+            tars_a2dp_connected =
+                false;
+
+            tars_status_text =
+                "A2DP CONNECTING";
+
+        }
+        else if (
+            state ==
+            ESP_A2D_CONNECTION_STATE_CONNECTED
+        ) {
+
+            tars_a2dp_connected =
+                true;
+
+            tars_a2dp_connecting =
+                false;
+
+            tars_status_text =
+                "A2DP CONNECTED";
+
+        }
+        else if (
+            state ==
+            ESP_A2D_CONNECTION_STATE_DISCONNECTING
+        ) {
+
+            tars_a2dp_connected =
+                false;
+
+            tars_a2dp_connecting =
+                false;
+
+            tars_status_text =
+                "A2DP DISCONNECTING";
+
+        }
+
+        return;
+
+    }
+
+
+    /* ---------------------------------------------
+       AUDIO STATE
+       --------------------------------------------- */
+
+    if (
+        event == ESP_A2D_AUDIO_STATE_EVT
+    ) {
+
+        esp_a2d_audio_state_t state =
+            param->audio_stat.state;
+
+
+        if (
+            state ==
+            ESP_A2D_AUDIO_STATE_STARTED
+        ) {
+
+            tars_status_text =
+                "A2DP AUDIO STREAMING";
+
+        }
+        else if (
+            state ==
+            ESP_A2D_AUDIO_STATE_STOPPED
+        ) {
+
+            if (
+                tars_a2dp_connected
             ) {
 
-                case ESP_A2D_CONNECTION_STATE_DISCONNECTED:
-
-                    tars_a2dp_connected =
-                        false;
-
-                    tars_a2dp_connecting =
-                        false;
-
-                    tars_status_text =
-                        "A2DP DISCONNECTED";
-
-                    break;
-
-
-                case ESP_A2D_CONNECTION_STATE_CONNECTING:
-
-                    tars_a2dp_connecting =
-                        true;
-
-                    tars_a2dp_connected =
-                        false;
-
-                    tars_status_text =
-                        "A2DP CONNECTING";
-
-                    break;
-
-
-                case ESP_A2D_CONNECTION_STATE_CONNECTED:
-
-                    tars_a2dp_connected =
-                        true;
-
-                    tars_a2dp_connecting =
-                        false;
-
-                    tars_status_text =
-                        "A2DP CONNECTED";
-
-                    break;
-
-
-                case ESP_A2D_CONNECTION_STATE_DISCONNECTING:
-
-                    tars_a2dp_connected =
-                        false;
-
-                    tars_a2dp_connecting =
-                        false;
-
-                    tars_status_text =
-                        "A2DP DISCONNECTING";
-
-                    break;
-
-
-                default:
-
-                    break;
+                tars_status_text =
+                    "A2DP CONNECTED";
 
             }
 
-            break;
+        }
+        else if (
+            state ==
+            ESP_A2D_AUDIO_STATE_REMOTE_SUSPEND
+        ) {
 
+            tars_status_text =
+                "A2DP AUDIO SUSPENDED";
 
-        /* ---------------------------------------------
-           AUDIO STATE
-           --------------------------------------------- */
+        }
 
-        case ESP_A2D_AUDIO_STATE_EVT:
-
-            switch (
-                param->audio_stat.state
-            ) {
-
-                case ESP_A2D_AUDIO_STATE_STARTED:
-
-                    tars_status_text =
-                        "A2DP AUDIO STREAMING";
-
-                    break;
-
-
-                case ESP_A2D_AUDIO_STATE_STOPPED:
-
-                    if (
-                        tars_a2dp_connected
-                    ) {
-
-                        tars_status_text =
-                            "A2DP CONNECTED";
-
-                    }
-
-                    break;
-
-
-                case ESP_A2D_AUDIO_STATE_REMOTE_SUSPEND:
-
-                    tars_status_text =
-                        "A2DP AUDIO SUSPENDED";
-
-                    break;
-
-
-                default:
-
-                    break;
-
-            }
-
-            break;
-
-
-        default:
-
-            break;
+        return;
 
     }
 
@@ -544,9 +559,7 @@ static mp_obj_t tars_a2dp_start(void) {
         BT_CONTROLLER_INIT_CONFIG_DEFAULT();
 
 
-    /* ---------------------------------------------
-       CONTROLLER INIT
-       --------------------------------------------- */
+    /* CONTROLLER INIT */
 
     ret =
         esp_bt_controller_init(
@@ -568,9 +581,7 @@ static mp_obj_t tars_a2dp_start(void) {
     }
 
 
-    /* ---------------------------------------------
-       CLASSIC BT ENABLE
-       --------------------------------------------- */
+    /* CLASSIC BT ENABLE */
 
     ret =
         esp_bt_controller_enable(
@@ -592,9 +603,7 @@ static mp_obj_t tars_a2dp_start(void) {
     }
 
 
-    /* ---------------------------------------------
-       BLUEDROID INIT
-       --------------------------------------------- */
+    /* BLUEDROID INIT */
 
     ret =
         esp_bluedroid_init();
@@ -614,9 +623,7 @@ static mp_obj_t tars_a2dp_start(void) {
     }
 
 
-    /* ---------------------------------------------
-       BLUEDROID ENABLE
-       --------------------------------------------- */
+    /* BLUEDROID ENABLE */
 
     ret =
         esp_bluedroid_enable();
@@ -636,9 +643,7 @@ static mp_obj_t tars_a2dp_start(void) {
     }
 
 
-    /* ---------------------------------------------
-       GAP CALLBACK
-       --------------------------------------------- */
+    /* GAP CALLBACK */
 
     ret =
         esp_bt_gap_register_callback(
@@ -660,9 +665,7 @@ static mp_obj_t tars_a2dp_start(void) {
     }
 
 
-    /* ---------------------------------------------
-       DEVICE NAME
-       --------------------------------------------- */
+    /* DEVICE NAME */
 
     ret =
         esp_bt_gap_set_device_name(
@@ -684,9 +687,7 @@ static mp_obj_t tars_a2dp_start(void) {
     }
 
 
-    /* ---------------------------------------------
-       DISCOVERABLE
-       --------------------------------------------- */
+    /* DISCOVERABLE */
 
     ret =
         esp_bt_gap_set_scan_mode(
@@ -709,9 +710,7 @@ static mp_obj_t tars_a2dp_start(void) {
     }
 
 
-    /* ---------------------------------------------
-       A2DP CALLBACK
-       --------------------------------------------- */
+    /* A2DP CALLBACK */
 
     ret =
         esp_a2d_register_callback(
@@ -733,9 +732,7 @@ static mp_obj_t tars_a2dp_start(void) {
     }
 
 
-    /* ---------------------------------------------
-       A2DP SOURCE INIT
-       --------------------------------------------- */
+    /* A2DP SOURCE INIT */
 
     ret =
         esp_a2d_source_init();
@@ -755,9 +752,7 @@ static mp_obj_t tars_a2dp_start(void) {
     }
 
 
-    /* ---------------------------------------------
-       AUDIO DATA CALLBACK
-       --------------------------------------------- */
+    /* AUDIO DATA CALLBACK */
 
     ret =
         esp_a2d_source_register_data_callback(
