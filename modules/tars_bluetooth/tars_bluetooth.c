4e4b83d0ebc410504a90cd82cce78662423bf070
hilmane34-a11y <hilmane34@gmail.com>
2026-08-30 14:11:26 +0700

    Refactor tars_bluetooth module structure

--- modules/tars_bluetooth/tars_bluetooth.c
+++ modules/tars_bluetooth/tars_bluetooth.c
@@ -1,10 +1,6 @@
 #include "py/runtime.h"
 
 
-/* =========================================================
-   TARS BLUETOOTH MODULE
-   ========================================================= */
-
 static mp_obj_t tars_bluetooth_test(void) {
     return mp_obj_new_str(
         "TARS BLUETOOTH READY",
@@ -12,18 +8,14 @@ static mp_obj_t tars_bluetooth_test(void) {
     );
 }
 
+
 static MP_DEFINE_CONST_FUN_OBJ_0(
     tars_bluetooth_test_obj,
     tars_bluetooth_test
 );
 
 
-/* =========================================================
-   MODULE TABLE
-   ========================================================= */
-
-static const mp_rom_map_elem_t
-tars_bluetooth_globals_table[] = {
+static const mp_rom_map_elem_t tars_bluetooth_globals_table[] = {
 
     {
         MP_ROM_QSTR(MP_QSTR___name__),
@@ -37,32 +29,23 @@ tars_bluetooth_globals_table[] = {
 
 };
 
+
 static MP_DEFINE_CONST_DICT(
     tars_bluetooth_globals,
     tars_bluetooth_globals_table
 );
 
 
-/* =========================================================
-   MODULE DEFINITION
-   ========================================================= */
-
-const mp_obj_module_t
-tars_bluetooth_user_cmodule = {
+const mp_obj_module_t tars_bluetooth_user_cmodule = {
 
     .base = {
         &mp_type_module
     },
 
-    .globals =
-        (mp_obj_dict_t *)&tars_bluetooth_globals,
+    .globals = (mp_obj_dict_t *)&tars_bluetooth_globals,
 
 };
 
-
-/* =========================================================
-   REGISTER MODULE
-   ========================================================= */
 
 MP_REGISTER_MODULE(
     MP_QSTR_tars_bluetooth,
