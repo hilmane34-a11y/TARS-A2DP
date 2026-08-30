397a8e00f2c2486194fe8660462b017666bc19af
hilmane34-a11y <hilmane34@gmail.com>
2026-08-30 16:20:24 +0700

    Refactor usermod_tars to usermod in CMake

--- modules/micropython.cmake
+++ modules/micropython.cmake
@@ -1,15 +1,9 @@
-add_library(usermod_tars INTERFACE)
-
-target_sources(usermod_tars INTERFACE
+target_sources(usermod INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/tars_a2dp/tars_a2dp.c
     ${CMAKE_CURRENT_LIST_DIR}/tars_bluetooth/tars_bluetooth.c
 )
 
-target_include_directories(usermod_tars INTERFACE
+target_include_directories(usermod INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/tars_a2dp
     ${CMAKE_CURRENT_LIST_DIR}/tars_bluetooth
-)
-
-target_link_libraries(usermod INTERFACE
-    usermod_tars
 )
