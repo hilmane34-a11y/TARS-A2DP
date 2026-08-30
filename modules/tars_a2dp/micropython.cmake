f20ab915d4ac69326986a503daff89074a9937bf
hilmane34-a11y <hilmane34@gmail.com>
2026-08-30 18:02:00 +0700

    Cleanup Bluetooth dependencies in micropython.cmake
    Removed Bluetooth ESP-IDF include and link directives.

--- modules/tars_a2dp/micropython.cmake
+++ modules/tars_a2dp/micropython.cmake
@@ -8,24 +8,6 @@ target_include_directories(usermod_tars_a2dp INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}
 )
 
-# Ambil include directory dari komponen Bluetooth ESP-IDF.
-# Ini diperlukan agar proses QSTR MicroPython dapat menemukan
-# header Bluetooth seperti esp_bt_main.h.
-idf_component_get_property(
-    TARS_BT_INCLUDE_DIRS
-    bt
-    INCLUDE_DIRS
-)
-
-target_include_directories(usermod_tars_a2dp INTERFACE
-    ${TARS_BT_INCLUDE_DIRS}
-)
-
-# Link ke library Bluetooth ESP-IDF.
-target_link_libraries(usermod_tars_a2dp INTERFACE
-    __idf_bt
-)
-
 target_link_libraries(usermod INTERFACE
     usermod_tars_a2dp
 )
