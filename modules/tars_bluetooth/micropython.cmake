3e034fef21c7ff6f11e138d65cef426b36ce03de
hilmane34-a11y <hilmane34@gmail.com>
2026-08-30 13:31:26 +0700

    Define MODULE_TARS_BLUETOOTH_ENABLED for Bluetooth module

--- modules/tars_bluetooth/micropython.cmake
+++ modules/tars_bluetooth/micropython.cmake
@@ -8,6 +8,10 @@ target_include_directories(usermod_tars_bluetooth INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}
 )
 
+target_compile_definitions(usermod_tars_bluetooth INTERFACE
+    MODULE_TARS_BLUETOOTH_ENABLED=1
+)
+
 target_link_libraries(usermod INTERFACE
     usermod_tars_bluetooth
 )
