5f8bc595ccf78bb753c5836bef626fa693e68ff4
hilmane34-a11y <hilmane34@gmail.com>
2026-08-30 14:09:24 +0700

    Refactor micropython.cmake to use INTERFACE library
    Replaced include directives with an INTERFACE library for usermod_tars, adding source files and include directories.

--- modules/micropython.cmake
+++ modules/micropython.cmake
@@ -1,2 +1,15 @@
-include(${CMAKE_CURRENT_LIST_DIR}/tars_a2dp/micropython.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/tars_bluetooth/micropython.cmake)
+add_library(usermod_tars INTERFACE)
+
+target_sources(usermod_tars INTERFACE
+    ${CMAKE_CURRENT_LIST_DIR}/tars_a2dp/tars_a2dp.c
+    ${CMAKE_CURRENT_LIST_DIR}/tars_bluetooth/tars_bluetooth.c
+)
+
+target_include_directories(usermod_tars INTERFACE
+    ${CMAKE_CURRENT_LIST_DIR}/tars_a2dp
+    ${CMAKE_CURRENT_LIST_DIR}/tars_bluetooth
+)
+
+target_link_libraries(usermod INTERFACE
+    usermod_tars
+)
