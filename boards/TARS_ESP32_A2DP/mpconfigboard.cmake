27a4692843263b88e022c8a9a56d685deaf58433
hilmane34-a11y <hilmane34@gmail.com>
2026-08-30 20:58:39 +0700

    Remove custom partition table configuration

--- boards/TARS_ESP32_A2DP/mpconfigboard.cmake
+++ boards/TARS_ESP32_A2DP/mpconfigboard.cmake
@@ -2,7 +2,3 @@ set(SDKCONFIG_DEFAULTS
     boards/sdkconfig.base
     ${MICROPY_BOARD_DIR}/sdkconfig.bluetooth
 )
-
-set(PARTITION_TABLE_CUSTOM_FILENAME
-    "${MICROPY_BOARD_DIR}/partitions.csv"
-)
