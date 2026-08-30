9336cd049256cd1cf55aa0593a4daaac2c47eb85
hilmane34-a11y <hilmane34@gmail.com>
2026-08-30 17:50:47 +0700

    Integrate Bluetooth ESP-IDF with A2DP module
    Added Bluetooth ESP-IDF include directory and linked library.

--- modules/tars_a2dp/micropython.cmake
+++ modules/tars_a2dp/micropython.cmake
@@ -8,7 +8,20 @@ target_include_directories(usermod_tars_a2dp INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}
 )
 
-# Hubungkan modul A2DP dengan komponen Bluetooth ESP-IDF
+# Ambil include directory dari komponen Bluetooth ESP-IDF.
+# Ini diperlukan agar proses QSTR MicroPython dapat menemukan
+# header Bluetooth seperti esp_bt_main.h.
+idf_component_get_property(
+    TARS_BT_INCLUDE_DIRS
+    bt
+    INCLUDE_DIRS
+)
+
+target_include_directories(usermod_tars_a2dp INTERFACE
+    ${TARS_BT_INCLUDE_DIRS}
+)
+
+# Link ke library Bluetooth ESP-IDF.
 target_link_libraries(usermod_tars_a2dp INTERFACE
     __idf_bt
 )
