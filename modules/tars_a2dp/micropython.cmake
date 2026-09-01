2b3fa358da4e80fa2c1daa6a92e3e19208ac1935
hilmane34-a11y <hilmane34@gmail.com>
2026-09-01 21:00:19 +0700

    Remove unused libraries from usermod_tars_a2dp

--- modules/tars_a2dp/micropython.cmake
+++ modules/tars_a2dp/micropython.cmake
@@ -10,10 +10,6 @@ target_include_directories(usermod_tars_a2dp INTERFACE
 
 target_link_libraries(usermod_tars_a2dp INTERFACE
     __idf_bt
-    __idf_esp_http_client
-    __idf_esp_tls
-    __idf_tcp_transport
-    __idf_mbedtls
 )
 
 target_link_libraries(usermod INTERFACE
