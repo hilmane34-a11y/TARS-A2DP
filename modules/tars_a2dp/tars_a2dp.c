d3764ee8bec86638fc5ad0b74298096a76a86801
hilmane34-a11y <hilmane34@gmail.com>
2026-09-01 21:58:05 +0700

    Refactor esp_http_client_config_t initialization

--- modules/tars_a2dp/tars_a2dp.c
+++ modules/tars_a2dp/tars_a2dp.c
@@ -2139,25 +2139,28 @@ tars_a2dp_tts(
 
 
     esp_http_client_config_t config =
-    {
-        .url =
-            TARS_CLOUD_TTS_URL,
+{
+    .url =
+        TARS_CLOUD_TTS_URL,
 
-        .method =
-            HTTP_METHOD_POST,
+    .method =
+        HTTP_METHOD_POST,
 
-        .event_handler =
-            tars_tts_http_event,
+    .event_handler =
+        tars_tts_http_event,
 
-        .timeout_ms =
-            30000,
+    .timeout_ms =
+        30000,
 
-        .buffer_size =
-            4096,
+    .buffer_size =
+        4096,
 
-        .buffer_size_tx =
-            4096
-    };
+    .buffer_size_tx =
+        4096,
+
+    .crt_bundle_attach =
+        esp_crt_bundle_attach
+};
 
 
     esp_http_client_handle_t client =
