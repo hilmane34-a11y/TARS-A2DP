8a6b25bf6ec59d424ba61d08c7d211ad7841b12a
hilmane34-a11y <hilmane34@gmail.com>
2026-09-02 20:48:15 +0700

    Adjust initial heap size for WiFi and HTTPS/TLS
    Increased initial heap size for better WiFi and HTTPS/TLS performance.

--- boards/TARS_ESP32_A2DP/mpconfigboard.h
+++ boards/TARS_ESP32_A2DP/mpconfigboard.h
@@ -1,6 +1,9 @@
 #define MICROPY_HW_BOARD_NAME "TARS ESP32 A2DP"
 #define MICROPY_HW_MCU_NAME "ESP32"
 
+// Sisakan lebih banyak RAM internal untuk WiFi dan HTTPS/TLS.
+#define MICROPY_GC_INITIAL_HEAP_SIZE (48 * 1024)
+
 // Matikan modul Bluetooth NimBLE bawaan MicroPython.
 // Bluetooth Classic/A2DP akan ditangani oleh modul C TARS.
 #define MICROPY_PY_BLUETOOTH (0)
