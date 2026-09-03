586fcc6765f11b0b813a73556ba4a40311997f4d
hilmane34-a11y <hilmane34@gmail.com>
2026-09-03 18:04:31 +0700

    Decrease initial heap size for MicroPython
    Reduced initial heap size for MicroPython from 35KB to 30KB.

--- boards/TARS_ESP32_A2DP/mpconfigboard.h
+++ boards/TARS_ESP32_A2DP/mpconfigboard.h
@@ -2,7 +2,7 @@
 #define MICROPY_HW_MCU_NAME "ESP32"
 
 // Sisakan lebih banyak RAM internal untuk WiFi dan HTTPS/TLS.
-#define MICROPY_GC_INITIAL_HEAP_SIZE (35 * 1024)
+#define MICROPY_GC_INITIAL_HEAP_SIZE (30 * 1024)
 
 // Matikan modul Bluetooth NimBLE bawaan MicroPython.
 // Bluetooth Classic/A2DP akan ditangani oleh modul C TARS.
